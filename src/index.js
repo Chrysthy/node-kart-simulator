ca92e7933d5bb7f5811018a9753f98215dd94a43
Chrystine Martins <chrystine.martins@outlook.com>
2025-06-07 18:28:45 -0300

    add game outputs

--- src/index.js
+++ src/index.js
@@ -18,9 +18,16 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+
 //Função auto-invocada
 (async function main() {
-    console.log("oi");
-    
+    console.log(
+        `🏁 Bem vindo(a) ao jogo de corrida!\n`
+    )
+
+    console.log(`👥 Corredores: ${player1.name} vs ${player2.name} \n`);
+
+    console.log(`🚦Começando a corrida...`);
+        
 })();
 
