3213722137bc5d37c46975b6e8f6a4e902f86d7c
Chrystine Martins <chrystine.martins@outlook.com>
2025-06-08 16:15:09 -0300

    remove score

--- src/personagens.js
+++ src/personagens.js
@@ -1,8 +1,8 @@
 export const personagens = [
-    { name: "Mario", speed: 4, handling: 3, power: 3, score: 0 },
-    { name: "Luigi", speed: 4, handling: 3, power: 3, score: 0 },
-    { name: "Peach", speed: 4, handling: 3, power: 3, score: 0 },
-    { name: "Yoshi", speed: 4, handling: 3, power: 3, score: 0 },
-    { name: "Bowser", speed: 4, handling: 3, power: 3, score: 0 },
-    { name: "Donkey Kong", speed: 4, handling: 3, power: 3, score: 0 }
+    { name: "Mario", speed: 4, handling: 3, power: 3 },
+    { name: "Luigi", speed: 4, handling: 3, power: 3 },
+    { name: "Peach", speed: 4, handling: 3, power: 3 },
+    { name: "Yoshi", speed: 4, handling: 3, power: 3 },
+    { name: "Bowser", speed: 4, handling: 3, power: 3 },
+    { name: "Donkey Kong", speed: 4, handling: 3, power: 3 }
 ];
